9e94ebc68bf6a36af7dc3249b64d1487c51944a3
jintianzhen <jtznenic@outlook.com>
2021-01-24 22:09:27 +0800

    测试

--- src/test/java/org/jtznenic/structural/proxy/JdkDynamicProxyTest.java
+++ src/test/java/org/jtznenic/structural/proxy/JdkDynamicProxyTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
+/**
+ * jdk 动态代理
+ */
 public class JdkDynamicProxyTest {
     @Test
     public void testProxy() {
